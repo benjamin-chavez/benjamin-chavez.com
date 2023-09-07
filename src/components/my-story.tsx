9035e0a5856819aea8c30769f4433c877e0d4c8e
Ben Chavez <ben.m.chavez@gmail.com>
2023-09-07 15:17:41 -0500

    updated my-stroy section links

--- src/components/my-story.tsx
+++ src/components/my-story.tsx
@@ -14,11 +14,24 @@ const storySection = {
       body: "I spent five years working in the financial services industry, where I loved the complex work and the demanding work environments, typically full of type A individuals. However, it wasn't long after starting in the industry that I noticed two things: first, our ability to translate human logic into computer code was quickly destroying and creating jobs; and second, many businesses use extremely antiquated software.",
     },
     {
-      body: 'It was with this knowledge that I realized that learning to code would inevitably be my next endeavor. After working as a senior financial analyst at Ayco, Goldman Sachs for three years, and soaking in as much knowledge as I could, I decided to take a leap. In January of 2020, I moved to Berlin, Germany to complete a 9-week full stack coding bootcamp at Le Wagon. It was during this bootcamp that I discovered that not only do I have a passion for the complexity of backend development, but that I really enjoy the creativity that comes with frontend development as well.',
+      body: 'It was with this knowledge that I realized that learning to code would inevitably be my next endeavor. After working as a senior financial analyst at Ayco, Goldman Sachs for three years, and soaking in as much knowledge as I could, I decided to take a leap. In January of 2020, I moved to Berlin, Germany to complete a 9-week full stack coding bootcamp at ',
+      link: {
+        href: 'https://www.lewagon.com/',
+        text: 'Le Wagon',
+      },
+      bodyAfterLink:
+        '. It was during this bootcamp that I discovered that not only do I have a passion for the complexity of backend development, but that I really enjoy the creativity that comes with frontend development as well.',
     },
     {
-      body: `Currently, I am looking for work in financial services, and once again, my mother's words are in my head: “Everything is a business”. Keeping this in mind, I am looking for a company with interesting problems where my skills in computer science can be used synergistically to benefit the business.",
-      'When I am not coding, you might find me producing/performing electronic music under the artist alias Amin Chavez, or reading. Here are a few books that changed me for the better:`,
+      body: `Currently, I am looking for work in financial services, and once again, my mother's words are in my head: “Everything is a business”. Keeping this in mind, I am looking for a company with interesting problems where my skills in computer science can be used synergistically to benefit the business.`,
+    },
+    {
+      body: 'When I am not coding, you might find me producing/performing electronic music under the artist alias ',
+      link: {
+        href: 'https://aminchavez.com/',
+        text: 'Amin Chavez',
+      },
+      bodyAfterLink: `, or reading. Here are a few books that changed me for the better:`,
     },
   ],
   books: [
@@ -45,6 +58,20 @@ const storySection = {
   ],
 };
 
+function TextLink({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <a target="_blank" href={href} className="underline hover:text-[#008000]">
+      {children}
+    </a>
+  );
+}
+
 export default function MyStory() {
   return (
     <section id="my-story" className="my-12 pt-4">
@@ -56,8 +83,21 @@ export default function MyStory() {
           <div className="font-open-sans text-[15px] font-light text-neutral-500">
             {storySection.paragraphs.map((paragraph, index) => (
               <p className="m-2" key={index}>
+                {' '}
                 {paragraph.body}
+                {paragraph.link && (
+                  <TextLink href={paragraph.link.href}>
+                    {paragraph.link.text}
+                  </TextLink>
+                )}
+                {paragraph.bodyAfterLink && paragraph.bodyAfterLink}
               </p>
+            ))}
+            {storySection.books.map((book) => (
+              <li className="m-2" key={book.title}>
+                <TextLink href={book.url}>{book.title}</TextLink> by{' '}
+                {book.author}
+              </li>
             ))}
           </div>
         </div>
