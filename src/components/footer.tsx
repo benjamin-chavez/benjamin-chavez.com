a8a9f2be41c1a855ca73c90579b0a7994cb7d7ca
Ben Chavez <ben.m.chavez@gmail.com>
2023-09-11 12:43:13 -0500

    testing footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,32 +1,13 @@
 // src/components/footer.tsx
-import 'server-only';
 import { Container } from '@/components/container';
-import { LinkedInIcon } from './icons/linkedin-icon';
-import { GithubIcon } from './icons/github-icon';
+import 'server-only';
 import EnvelopeFilledIcon from './icons/envelope-filled-icon';
+import { GithubIcon } from './icons/github-icon';
+import { LinkedInIcon } from './icons/linkedin-icon';
 
-import BootstrapIcon from './icons/bootstrap-icon';
-import CssIcon from './icons/css-icon';
-import DatabaseIcon from './icons/database-icon';
-import ExcelIcon from './icons/excel-icon';
-import FigmaIcon from './icons/figma-icon';
-import GitIcon from './icons/git-icon';
-import HtmlIcon from './icons/html-icon';
-import JavaIcon from './icons/java-icon';
 import JavaScriptIcon from './icons/javascript-icon';
-import LinuxIcon from './icons/linux-icon';
-import MacIcon from './icons/mac-icon';
-import NextjsIcon from './icons/nextjs-icon';
-import NodeIcon from './icons/node-icon';
-import PostgresIcon from './icons/postgres-icon';
-import PythonIcon from './icons/python-icon';
-import RailsIcon from './icons/rails-icon';
-import ReactIcon from './icons/react-icon';
-import ReduxIcon from './icons/redux-icon';
 import RubyIcon from './icons/ruby-icon';
-import TailwindIcon from './icons/tailwind-icon';
 import TypescriptIcon from './icons/typescript-icon';
-import WindowsIcon from './icons/windows-icon';
 
 const technologies = {
   'Languages & Technologies': [
@@ -64,26 +45,15 @@ export default function Footer() {
             Technologies
           </h2>
         </div>
-        <div className="my-6 flex flex-col items-center  justify-center gap-8 font-open-sans ">
-          {Object.entries(technologies).map(([category, techs]) => {
+        <div className="my-6 flex items-center  justify-center gap-8 font-open-sans ">
+          {contactLinks.map((contact) => {
+            const { IconComponent } = contact;
             return (
               <div
-                key={category}
+                key={contact.title}
                 className="flex w-full flex-wrap items-center justify-center gap-5  "
               >
-                {techs.map((tech) => {
-                  const { IconComponent } = tech;
-
-                  return (
-                    <div
-                      key={tech.title}
-                      className="flex items-center whitespace-nowrap text-base text-white"
-                    >
-                      <IconComponent />
-                      <span className="ml-1">{tech.title}</span>
-                    </div>
-                  );
-                })}
+                <IconComponent className=" h-[30px] w-full text-neutral-500 hover:text-white" />
               </div>
             );
           })}
